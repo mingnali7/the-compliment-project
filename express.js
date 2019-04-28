1f6da8ea180d243e5cf764f6b86e66c35dd4b2a2
Mingna Li <mingnali@10-17-154-74.dynapool.wireless.nyu.edu>
2019-04-28 00:00:11 -0400

    improved backend

--- express.js
+++ express.js
@@ -1,22 +1,82 @@
+var {
+  Client
+} = require('pg');
 let express = require("express")
+var mustacheExpress = require('mustache-express');
 let path = require('path')
+var bodyParser = require('body-parser')
 
+// make an array that has 10 compliments
+let compArray = ["you're smart", "you're beautiful", "you're handsome"];
+
+// establish the express thing
 let app = express();
+
+// body parser stuff
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}));
+
+// establish the mustache thing
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname);
+
+// getting database
+var client = new Client({
+  database: 'compliment'
+  // connectionString: process.env.DATABASE_URL,
+  // ssl: true,
+});
+
+client.connect();
+
 
 
 app.get("/", function (req, res) {
+
   res.sendFile(path.join(__dirname + '/1allure.html'))
 })
 
 
 app.get("/2", function (req, res) {
-  res.sendFile(path.join(__dirname + '/2compliment.html'))
+  client.query("SELECT message FROM posts;", function (err, res1) {
+    if (err) {
+      console.log(err)
+    }
+    let randomNum = Math.floor(Math.random() * res1.rows.length)
+    let oldComp = res1.rows[randomNum];
+    // console.log(oldComp)
+    // const keys = Object.keys(oldComp);
+    // const randomIndex = keys[Math.floor(Math.random() * keys.length)];
+    // const item = oldComp[randomIndex];
+    console.log(oldComp);
+    // client.end()
+    res.render('2compliment', {
+      old: oldComp
+    })
+  })
+  //pick a random index for the compArray
+  // let randomNum = Math.floor(Math.random() * compArray.length);
+  // let compliment = compArray[randomNum];
+  // // feed the random compliment into {{compliment}} in 2compliment.html
+  // res.render('2compliment', {
+  //   compliment: compliment
+  // })
+  // res.sendFile(path.join(__dirname + '/2compliment.html'))
 })
 
 app.get("/3", function (req, res) {
   res.sendFile(path.join(__dirname + '/3question.html'))
 })
 
+app.post("/post", function (req, res) {
+  console.log(req.body.newcomp)
+  res.render('4typing', {
+    post: req.body.newcomp
+  })
+})
 app.get("/4", function (req, res) {
   res.sendFile(path.join(__dirname + '/4typing.html'))
 })
